f23d3915d11bc5784acf52ad9429e8c1292d9283
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-04-16 10:03:01 +0100

    Created brand sanity schema

---
+++ cn_wears_sanity/schemaTypes/brands.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'brands',
+  title: 'Brands',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name of Brand',
+      type: 'string',
+    },
+    {
+      name: 'logo',
+      title: 'Logo of Brand',
+      type: 'image',
+    },
+  ],
+}
--- cn_wears_sanity/schemaTypes/index.ts
+++ cn_wears_sanity/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import category from './category'
 import products from './products'
 import hero_images from './hero-images'
 import label from './label'
+import brands from './brands'
 
-export const schemaTypes = [products, category, hero_images, label]
+export const schemaTypes = [products, category, hero_images, label, brands]
