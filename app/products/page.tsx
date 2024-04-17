78a163cfb430afb73606c7e54de1296cb5698570
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-04-17 19:22:53 +0100

    Added product sort component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -7,6 +7,17 @@ import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
 import { container } from "@/components/ui/container";
 
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+
 export const dynamic = "force-dynamic";
 
 type Props = {};
@@ -32,9 +43,28 @@ const ProductsPage = async (props: Props) => {
 	const products: Product[] = await fetchAllProducts();
 	return (
 		<div className={`${container} mt-40  `}>
-			<div className='mb-20'>
-				<h1 className='text-5xl font-semibold text-center'>Our Products</h1>
+			<div className=' flex justify-between items-center'>
+				<h1 className='text-xl font-semibold '>
+					{products.length} {products.length < 2 ? "product" : "products"}
+				</h1>
+
+				<Select>
+					<SelectTrigger className='w-[180px]'>
+						<SelectValue placeholder='Sort By' />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectLabel>Sort By</SelectLabel>
+							<SelectItem value='date:desc'>Newest</SelectItem>
+							<SelectItem value='date:asc'>Oldest</SelectItem>
+							<SelectItem value='price:desc'>Price: Low to High</SelectItem>
+							<SelectItem value='price:asc'>Price: High to low</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
 			</div>
+
+			<Separator className='my-10' />
 
 			<div className='grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 xl:gap-x-12 xl:gap-y-16 xl:grid-cols-3 '>
 				{products.map((product) => (
